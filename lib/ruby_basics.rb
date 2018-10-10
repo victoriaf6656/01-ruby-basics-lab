3a25e5e6e5ffb8db1a094e3f1a64f4610496e9ea
Victoria Feng <victoriaf6656@hstat.org>
2018-10-10 13:12:45 +0000

    corrections

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,23 +1,29 @@
-def division
+def division(num, num2)
+    puts "First Num(low): "
+    num = gets.to_f
+    puts "Second Num(High): "
+    num2 = gets.to_f
+    total = num2/num
+    return total
+end
+
+def argue(receive)
+  receive
+end
+
+def greeting(greeting,name)
     
-end
-
-def argue
-  
-end
-
-def greeting
-
 end
 
 def return_a_value
-    
+    return "Nice"
 end
 
-def pizza_party
-    
+def pizza_party(flavor="cheese",flavor2="pepperoni")
+    return flavor
+    return flavor2
 end
 
-def whisper
-    
+def whisper(say)
+    say.downcase
 end
